2c2a8208f61dd7468ce30b6441a9f18eed4f1a98
Kerim_Lex <lexmeins1@gmail.com>
2025-06-25 01:13:56 +0500

    initial commit

---
+++ Data/UserDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class UserDbContext : IdentityDbContext<User>
+    {
+        public UserDbContext(Microsoft.EntityFrameworkCore.DbContextOptions<UserDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(Microsoft.EntityFrameworkCore.DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=users.db");
+            }
+        }
+    }
+}
